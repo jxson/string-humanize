61d6ee8aff8bfe2a5e2c238fc1eeb83950b5b453
Jason Campbell <jason@artifact.sh>
2013-11-04 22:26:36 -0800

    Adds initial stubs

---
+++ index.js
@@ -0,0 +1,6 @@
+
+module.exports = humanize
+
+function humanize(string){
+  return string
+}
---
+++ test-humanize.js
@@ -0,0 +1,14 @@
+
+const assert = require('assert')
+const humanize = require('./index.js')
+
+describe('humanize(string)', function(){
+  it('converts snake case')
+  it('converts camel case')
+  it('converts hyphen separators')
+  it('removes extensions')
+  it('capitalizes lower case phrases')
+  it('zaps extra spaces')
+  it('coerces numbers')
+  it('handles bad input gracefully')
+})
