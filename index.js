c67fcc2ae3670f6130e1864d6cbc1740253ef22e
Jason Campbell <jason@artifact.sh>
2013-11-05 23:51:06 -0800

    Adds working tests and code

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
 
+const capitalize = require('string-capitalize')
+
 module.exports = humanize
 
 function humanize(string){
+  string = string || ''
+  string = string.toString() // might be a number
+  string = string.trim()
+  string = string.replace(extname(string), '')
+  string = underscore(string)
+  string = string.replace(/[\W_]+/g, ' ')
+  return capitalize(string)
+}
+
+
+function underscore(string){
+  string = string || ''
+  string = string.toString() // might be a number
+  string = string.trim()
+  string = string.replace(/([a-z\d])([A-Z]+)/g, '$1_$2')
+  string = string.replace(/[-\s]+/g, '_').toLowerCase()
   return string
+}
+
+function extname(string){
+  var index = string.lastIndexOf('.')
+    , ext = string.substring(index, string.length)
+
+  return (index === -1) ? '' : ext
 }
--- test-humanize.js
+++ test-humanize.js
@@ -3,12 +3,37 @@ const assert = require('assert')
 const humanize = require('./index.js')
 
 describe('humanize(string)', function(){
-  it('converts snake case')
-  it('converts camel case')
-  it('converts hyphen separators')
-  it('removes extensions')
-  it('capitalizes lower case phrases')
-  it('zaps extra spaces')
-  it('coerces numbers')
-  it('handles bad input gracefully')
+  it('converts snake case', function(){
+    assert.equal(humanize('super_snake_case'), 'Super snake case')
+  })
+
+  it('converts camel case', function(){
+    assert.equal(humanize('capitalizedCamelCase'), 'Capitalized camel case')
+  })
+
+  it('converts hyphen separators', function(){
+    assert.equal(humanize('hyphen-case'), 'Hyphen case')
+  })
+
+  it('removes extensions', function(){
+    assert.equal(humanize('no-extensions-here.md'), 'No extensions here')
+  })
+
+  it('capitalizes lower case phrases', function(){
+    assert.equal(humanize('lower cased phrase'), 'Lower cased phrase')
+  })
+
+  it('zaps extra spaces', function(){
+    assert.equal(humanize('  so many  spaces  '), 'So many spaces')
+  })
+
+  it('coerces numbers', function(){
+    assert.equal(humanize(123), '123')
+  })
+
+  it('handles bad input gracefully', function(){
+    assert.equal(humanize(''), '')
+    assert.equal(humanize(null), '')
+    assert.equal(humanize(undefined), '')
+  })
 })
